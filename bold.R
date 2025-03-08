2756d92dcabef4e8f73b90aea455f4c8ea541fc1
Nadador-max <gerar.bio14@gmail.com>
2025-03-08 18:02:14 -0500

    Update bold.R

--- bold.R
+++ bold.R
@@ -1,3 +1,4 @@
+################INSTALAR Y CARGAR PAQUETES NECESARIOS#########################
 install.packages("dplyr")
 install.packages("tidyverse")
 install.packages("tidyr")
@@ -16,10 +17,18 @@ library(purrr)
 library(parallel)
 library(foreach)
 library(doParallel)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
 
 ##############################################################################
+####ESCOGER CARPETA DE TRABAJO################################################
 setwd("C:/Users/GERARDO/Desktop/bold")
-#JALAR DATOS DEL BOLD
+
+
+##############################################################################
+#IMPORTAR DATOS BOLD EN FORMATO TSV###########################################
+#01 SOLO GRUPO
 datos_pilum <- bold_seqspec(taxon = "Pilumnus", format = "tsv")
 datos_micro <- bold_seqspec(taxon = "Microphrys", format = "tsv")
 datos_acan <- bold_seqspec(taxon = "Acanthonyx petiverii", format = "tsv")
@@ -28,17 +37,16 @@ datos_bal <- bold_seqspec(taxon = "Balanus", format = "tsv")
 datos_ste <- bold_seqspec(taxon = "Stenothoe", format = "tsv")
 datos_syna <- bold_seqspec(taxon = "Synalpheus", format = "tsv")
 
-taxones <- c("Pilumnus", "Microphrys", "Acanthonyx petiverii", 
-             "Conopea", "Balanus", "Stenothoe", "Synalpheus")
-
+#VARIOS GRUPOS
 datos<- bold_seqspec(taxon = c("Pilumnus", "Microphrys", "Acanthonyx petiverii", 
                                "Conopea", "Balanus", "Stenothoe", 
                                "Synalpheus"), format = "tsv")
 
 
-#ESCOGER LAS COLUMNAS DE MI INTERES
+###############################################################################
+#ESCOGER LA DATA DE MI INTERES#################################################
 datos_coi5p <- datos %>%
-  filter(markercode == "COI-5P") %>%
+  filter(markercode == "COI-5P") %>% #INDICAR EL MARCADOR DE INTRES
   mutate(valid_nucleotide_count = str_count(nucleotides, "[^-]")) %>%  # Contar solo caracteres distintos de "-"
   filter(valid_nucleotide_count > 500) %>%  # Filtrar por cantidad de caracteres sin contar guiones
   select(processid,
@@ -50,7 +58,9 @@ datos_coi5p <- datos %>%
          trace_names,
          nucleotides)
 
-#SACAR DATOS DEL NCBI#####
+
+##################################################################################
+#IMPORTAR EL GenBank ID DEL NCBI USANDO EL "genbank_accession" ###########################
 # Función para obtener el ID de GenBank desde el número de acceso
 get_genbank_id <- function(accession) {
   if (is.na(accession) | accession == "") {
@@ -70,17 +80,15 @@ get_genbank_id <- function(accession) {
   }
 }
 
-
-
 #Obtener GenBank ID para cada número de acceso en la columna "genbank_accession"
-
 datos_coi5p$genbank_id <- sapply(datos_coi5p$genbank_accession, get_genbank_id)
 
-sum(!is.na(datos_coi5p$genbank_id))
 
 
 
 
+################################################################################
+#OBTENER DATA DEL FORMATO GENBANK DE LAS SECUENCIAS DE MI INTERES###############
 # Filtrar solo IDs válidos (sin NA ni valores vacíos)
 valid_ids <- na.omit(datos_coi5p$genbank_id)
 valid_ids <- valid_ids[valid_ids != ""]  # Quitar valores vacíos
@@ -89,6 +97,7 @@ valid_ids <- valid_ids[valid_ids != ""]  # Quitar valores vacíos
 # Inicializar las nuevas columnas en datos_coi5p con NA
 datos_coi5p$Title <- NA
 datos_coi5p$Authors <- NA
+datos_coi5p$Journal <- NA
 
 # Recorrer solo los IDs válidos
 for (i in seq_along(valid_ids)) {
@@ -119,11 +128,28 @@ for (i in seq_along(valid_ids)) {
     authors_line <- lines[grep("^  AUTHORS", lines)]
     authors <- ifelse(length(authors_line) > 0, sub("^  AUTHORS +", "", authors_line[1]), NA)
     
+    # Extraer el primer JOURNAL
+    journal_line <- lines[grep("^  JOURNAL", lines)]
+    journal <- ifelse(length(journal_line) > 0, sub("^  JOURNAL +", "", journal_line[1]), NA)
+    
+    
     # Guardar resultados en las columnas de datos_coi5p (asignar en la fila correcta)
     datos_coi5p$Title[datos_coi5p$genbank_id == id] <- title
     datos_coi5p$Authors[datos_coi5p$genbank_id == id] <- authors
+    datos_coi5p$Journal[datos_coi5p$genbank_id == id] <- journal
   }
 }
 
-write.csv(datos_coi5p, "datos_coi5p.csv", row.names = FALSE)
+
+##########################################################################
+####FILTRAR SECUENCIAS ASOCIADAS A ARTICULOS CIENTIFICOS##################
+datos_coi5p_fil <- datos_coi5p %>%
+  filter(!(trace_names == "" & Journal == "Unpublished"))
+
+
+##########################################################################
+###########EXPORTAR EN FORMATO EXCEL######################################
+write.csv(datos_coi5p_fil, "datos_coi5p_fil.csv", row.names = FALSE) 
+
+
 
